00d0bd17d5b3e2f3363a7e430c21a6a1987a2fd7
Akshat Patel <akshat@Akshats-MacBook-Air.local>
2025-07-27 13:59:36 +0530

    Express Base Setup

---
+++ index.js
@@ -0,0 +1,26 @@
+require('dotenv').config();
+
+//syntax for importing express , make an object
+const express = require('express');
+
+//app is an instance of express
+const app = express();
+
+//port number
+//Prod base application we use dot env , we don't directly define it here.
+// const PORT = 3000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World! This is a basic Express server.');
+}
+);
+
+app.get('/about', (req, res) => {  res.send('This is the About page. Here you can learn more about us.'); }
+);
+
+
+//listening to the server on PORT 3000
+app.listen(process.env.PORT, () => {
+    console.log(`Server is running on http://localhost:${process.env.PORT}`);
+}
+);
